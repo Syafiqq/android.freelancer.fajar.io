8c7e79612605c74b687a0057f386634a861e907a
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-13 13:32:22 +0700

    Completing dashboard

--- app/src/main/java/io/localhost/freelancer/statushukum/controller/Dashboard.java
+++ app/src/main/java/io/localhost/freelancer/statushukum/controller/Dashboard.java
@@ -2,10 +2,12 @@ package io.localhost.freelancer.statushukum.controller;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -18,14 +20,21 @@ import com.jaredrummler.materialspinner.MaterialSpinner;
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import java.sql.SQLException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import io.localhost.freelancer.statushukum.R;
+import io.localhost.freelancer.statushukum.controller.adapter.CountPerYearAdapter;
 import io.localhost.freelancer.statushukum.model.database.model.MDM_Data;
 
 public class Dashboard extends AppCompatActivity
 {
     public static final String CLASS_NAME = "Dashboard";
     public static final String CLASS_PATH = "io.localhost.freelancer.statushukum.controller.Dashboard";
+    private CountPerYearAdapter recycleViewAdapter;
 
     private static void doSync()
     {
@@ -42,6 +51,7 @@ public class Dashboard extends AppCompatActivity
         setContentView(R.layout.activity_dashboard);
 
         this.setToolbar();
+        this.setYearList();
         this.setCenturyList();
 
         final MDM_Data model_data = MDM_Data.getInstance(this);
@@ -55,6 +65,16 @@ public class Dashboard extends AppCompatActivity
         {
             Log.i(CLASS_NAME, CLASS_PATH + ".SQLException");
         }
+    }
+
+    private void setYearList()
+    {
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.content_dashboard_recycle_view_container);
+        this.recycleViewAdapter = new CountPerYearAdapter(new ArrayList<MDM_Data.CountPerYear>(0), this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(super.getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(this.recycleViewAdapter);
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -79,11 +99,37 @@ public class Dashboard extends AppCompatActivity
         spinner.setItems(spinnerItem);
         spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<CharSequence>()
         {
-
             @Override
             public void onItemSelected(MaterialSpinner view, int position, long id, CharSequence item)
             {
-                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+                try
+                {
+                    final MDM_Data                    modelData = MDM_Data.getInstance(Dashboard.this);
+                    final int                         yearStart = NumberFormat.getInstance(Locale.getDefault()).parse(item.toString()).intValue() * 100;
+                    final int                         yearEnd   = yearStart + 99;
+                    final List<MDM_Data.CountPerYear> dbResult  = modelData.getCountPerYear(yearStart, yearEnd);
+                    final List<MDM_Data.CountPerYear> result    = new ArrayList<>(yearEnd - yearStart + 1);
+                    int                               iYear     = yearStart - 1;
+                    while(!dbResult.isEmpty())
+                    {
+                        final MDM_Data.CountPerYear tmpCPY = dbResult.remove(0);
+                        for(int isYear = tmpCPY.getYear(); ++iYear < isYear; )
+                        {
+                            result.add(new MDM_Data.CountPerYear(iYear, 0));
+                        }
+
+                        result.add(tmpCPY);
+                    }
+                    for(int isYear = yearEnd + 1; ++iYear < isYear; )
+                    {
+                        result.add(new MDM_Data.CountPerYear(iYear, 0));
+                    }
+                    Dashboard.this.recycleViewAdapter.update(result);
+                }
+                catch(ParseException ignored)
+                {
+                    Log.i(CLASS_NAME, "ParseException");
+                }
             }
         });
     }
