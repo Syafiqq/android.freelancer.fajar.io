6f40419f569296da9cad416ded8191b124e55e13
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-13 07:21:15 +0700

    Add setting

---
+++ app/src/main/java/io/localhost/freelancer/statushukum/controller/Setting.java
@@ -0,0 +1,27 @@
+package io.localhost.freelancer.statushukum.controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import io.localhost.freelancer.statushukum.R;
+
+public class Setting extends AppCompatActivity
+{
+    public static final String CLASS_NAME = "Setting";
+    public static final String CLASS_PATH = "io.localhost.freelancer.statushukum.controller.Setting";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Log.i(CLASS_NAME, CLASS_PATH + ".onCreate");
+
+        setContentView(R.layout.activity_setting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_setting_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
