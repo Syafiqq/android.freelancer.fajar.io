c6d5927ff58ce9874afaf561972d4f2d37c52280
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-16 22:53:20 +0700

    add null paramter to sync

--- app/src/main/java/io/localhost/freelancer/statushukum/controller/Setting.java
+++ app/src/main/java/io/localhost/freelancer/statushukum/controller/Setting.java
@@ -57,7 +57,7 @@ public class Setting extends AppCompatActivity
                         @Override
                         protected Void doInBackground(Void... voids)
                         {
-                            Setting.this.setting.doSync();
+                            Setting.this.setting.doSync(null);
                             return null;
                         }
 
