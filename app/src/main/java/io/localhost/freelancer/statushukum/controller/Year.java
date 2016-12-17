b3adde07fbe4bc7ba0679ba80d513b10ccc0f56d
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-17 07:08:45 +0700

    reworked year page

--- app/src/main/java/io/localhost/freelancer/statushukum/controller/Year.java
+++ app/src/main/java/io/localhost/freelancer/statushukum/controller/Year.java
@@ -9,18 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
 import io.localhost.freelancer.statushukum.R;
 import io.localhost.freelancer.statushukum.controller.adapter.YearListAdapter;
+import io.localhost.freelancer.statushukum.controller.filter.YearListFilter;
 import io.localhost.freelancer.statushukum.model.database.model.MDM_Data;
 import io.localhost.freelancer.statushukum.model.database.model.MDM_DataTag;
 import io.localhost.freelancer.statushukum.model.database.model.MDM_Tag;
@@ -34,8 +40,11 @@ public class Year extends AppCompatActivity
     public static final String EXTRA_YEAR_SIZE = "count";
     private int             year;
     private int             yearSize;
-    private YearListAdapter yearList;
+    private YearListAdapter yearListAdapter;
     private boolean isSyncOperated = false;
+    private List<MDM_Data.YearMetadata> entryList;
+    private SearchView                  search;
+
 
     private void doSync()
     {
@@ -45,10 +54,51 @@ public class Year extends AppCompatActivity
             this.isSyncOperated = true;
             new AsyncTask<Void, Void, Void>()
             {
+                private Observer callback;
+
+                @Override
+                protected void onPreExecute()
+                {
+                    callback = new Observer()
+                    {
+                        @Override
+                        public void update(final Observable observable, final Object o)
+                        {
+                            Year.super.runOnUiThread(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    switch((Integer) o)
+                                    {
+                                        case io.localhost.freelancer.statushukum.model.util.Setting.SYNC_FAILED:
+                                        {
+                                            Toast.makeText(Year.this, Year.super.getString(R.string.system_setting_server_version_error), Toast.LENGTH_SHORT).show();
+                                        }
+                                        break;
+                                        case io.localhost.freelancer.statushukum.model.util.Setting.SYNC_SUCCESS:
+                                        {
+                                            Toast.makeText(Year.this, Year.super.getString(R.string.system_setting_server_version_success), Toast.LENGTH_SHORT).show();
+                                            Year.this.setYearList();
+                                        }
+                                        break;
+                                        case io.localhost.freelancer.statushukum.model.util.Setting.SYNC_EQUAL:
+                                        {
+                                            Toast.makeText(Year.this, Year.super.getString(R.string.system_setting_server_version_equal), Toast.LENGTH_SHORT).show();
+                                        }
+                                        break;
+                                    }
+                                }
+                            });
+                        }
+                    };
+                    super.onPreExecute();
+                }
+
                 @Override
                 protected Void doInBackground(Void... voids)
                 {
-                    io.localhost.freelancer.statushukum.model.util.Setting.getInstance(Year.this).doSync(null);
+                    io.localhost.freelancer.statushukum.model.util.Setting.getInstance(Year.this).doSync(this.callback);
                     return null;
                 }
 
@@ -73,8 +123,41 @@ public class Year extends AppCompatActivity
         this.yearSize = intent.getIntExtra(Year.EXTRA_YEAR_SIZE, -1);
 
         this.setToolbar();
+        this.setProperty();
         this.setYearListAdapter();
         this.setYearList();
+    }
+
+    private void setProperty()
+    {
+        Log.i(CLASS_NAME, CLASS_PATH + ".setProperty");
+
+        if(this.entryList == null)
+        {
+            this.entryList = new LinkedList<>();
+        }
+        else
+        {
+            this.entryList.clear();
+        }
+        this.setYearListAdapter();
+        this.setYearList();
+        this.search = (SearchView) super.findViewById(R.id.content_year_search_filter);
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                Year.this.yearListAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
     private void setToolbar()
@@ -157,11 +240,12 @@ public class Year extends AppCompatActivity
         Log.i(CLASS_NAME, CLASS_PATH + ".setYearListAdapter");
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.content_year_recycle_view_container);
-        this.yearList = new YearListAdapter(new ArrayList<MDM_Data.YearMetadata>(0), this);
+        this.yearListAdapter = new YearListAdapter(new ArrayList<MDM_Data.YearMetadata>(0), this);
+        this.yearListAdapter.setFilter(new YearListFilter(this.yearListAdapter, this.entryList));
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(super.getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(this.yearList);
+        recyclerView.setAdapter(this.yearListAdapter);
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -169,22 +253,39 @@ public class Year extends AppCompatActivity
     {
         Log.i(CLASS_NAME, CLASS_PATH + ".setYearList");
 
-        final MDM_Data                    modelData    = MDM_Data.getInstance(this);
-        final MDM_DataTag                 modelDataTag = MDM_DataTag.getInstance(this);
-        final MDM_Tag                     modelTag     = MDM_Tag.getInstance(this);
-        final List<MDM_Data.YearMetadata> dbResultData = modelData.getYearList(this.year, this.yearSize);
-        final Map<Integer, ME_Tag>        dbResultTag  = modelTag.getAll();
-        for(final MDM_Data.YearMetadata result : dbResultData)
+        new AsyncTask<Void, Void, Void>()
         {
-            if(result.getTagSize() > 0)
+            @Override
+            protected Void doInBackground(Void... voids)
             {
-                final List<Integer> dbResultTagID = modelDataTag.getTagFromDataID(result.getId());
-                for(int tagId : dbResultTagID)
+                final MDM_Data                    modelData    = MDM_Data.getInstance(Year.this);
+                final MDM_DataTag                 modelDataTag = MDM_DataTag.getInstance(Year.this);
+                final MDM_Tag                     modelTag     = MDM_Tag.getInstance(Year.this);
+                final List<MDM_Data.YearMetadata> dbResultData = modelData.getYearList(Year.this.year, Year.this.yearSize);
+                final Map<Integer, ME_Tag>        dbResultTag  = modelTag.getAll();
+                for(final MDM_Data.YearMetadata result : dbResultData)
                 {
-                    result.add(dbResultTag.get(tagId));
+                    if(result.getTagSize() > 0)
+                    {
+                        final List<Integer> dbResultTagID = modelDataTag.getTagFromDataID(result.getId());
+                        for(int tagId : dbResultTagID)
+                        {
+                            result.add(dbResultTag.get(tagId));
+                        }
+                    }
                 }
+                Year.this.entryList.clear();
+                Year.this.entryList.addAll(dbResultData);
+                Year.this.yearListAdapter.update(dbResultData);
+                return null;
             }
-        }
-        this.yearList.update(dbResultData);
+
+            @Override
+            protected void onPostExecute(Void aVoid)
+            {
+                Year.this.search.setQuery(Year.this.search.getQuery(), true);
+                super.onPostExecute(aVoid);
+            }
+        }.execute();
     }
 }
