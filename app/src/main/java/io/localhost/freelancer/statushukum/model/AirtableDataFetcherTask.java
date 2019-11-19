f091b390ff46eca55a3da4c38dc39ad45c8a0701
Syafiq <syafiq.rezpector@gmail.com>
2019-11-19 22:19:21 +0700

    add airtable data fetcher task

---
+++ app/src/main/java/io/localhost/freelancer/statushukum/model/AirtableDataFetcherTask.java
@@ -0,0 +1,24 @@
+package io.localhost.freelancer.statushukum.model;
+
+import android.os.AsyncTask;
+
+import com.android.volley.RequestQueue;
+
+public class AirtableDataFetcherTask extends AsyncTask<Void, Void, AirtableDataFetcher> implements AirtableDataFetcher.InteractionListener {
+    AirtableDataFetcher fetcher;
+
+    public AirtableDataFetcherTask(RequestQueue queue) {
+        fetcher = new AirtableDataFetcher(queue, this);
+    }
+
+    @Override
+    protected AirtableDataFetcher doInBackground(Void... voids) {
+        return fetcher.doInBackground(voids);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        fetcher.onPreExecute();
+    }
+}
