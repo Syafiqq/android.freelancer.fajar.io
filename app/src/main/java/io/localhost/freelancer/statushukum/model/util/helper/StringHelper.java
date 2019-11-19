75a418b2c11186f9c8491329e5067bce178aba66
Syafiq <syafiq.rezpector@gmail.com>
2019-11-19 07:42:02 +0700

    add capture single value

--- app/src/main/java/io/localhost/freelancer/statushukum/model/util/helper/StringHelper.java
+++ app/src/main/java/io/localhost/freelancer/statushukum/model/util/helper/StringHelper.java
@@ -2,6 +2,9 @@ package io.localhost.freelancer.statushukum.model.util.helper;
 
 import android.text.TextUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringHelper {
     public static String trimOrNull(String s) {
         if(s == null)
@@ -12,5 +15,17 @@ public class StringHelper {
                 return null;
             return t;
         }
+    }
+
+    public static String captureSingleValue(String value, Pattern pattern) {
+        final Matcher m = pattern.matcher(value);
+
+        if (m.find()) {
+            try {
+                return m.group(1);
+            } catch (Exception ignored) {
+            }
+        }
+        return null;
     }
 }
